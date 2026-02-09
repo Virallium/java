bd2e3008bd4cf1d29c3386990e8ea90239250f62
Miradi <mengimiradit@gmail.com>
2026-02-09 22:25:51 +0100

    first commit

---
+++ src/smk_supermarché/SMK_SuperMarché.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package smk_supermarché;
+
+/**
+ *
+ * @author mengi
+ */
+public class SMK_SuperMarché {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
